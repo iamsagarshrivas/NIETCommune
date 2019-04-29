b50f76270aeb69924bc3a06ac1fbf20fb68bc72e
sagar.shrivas <sagar.shrivas@geminisolutions.com>
2019-04-29 17:23:33 +0530

    changes path response

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -7,6 +7,7 @@ router.get('/', userController.getAllUser);
 router.get('/another',userController.getAnotherUser);
 router.post('/login',userController.authenticateUser);
 router.post('/register-user',userController.registerUser);
+router.post('/update-user',userController.updateUser);
 router.patch('/register-user',userController.updateUser);
 
 module.exports = router;
