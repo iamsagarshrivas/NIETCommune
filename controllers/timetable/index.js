6b0a34e41fc2ae22bd13f788769552476ce3fa34
sagar.shrivas <sagar.shrivas@geminisolutions.com>
2019-05-04 00:48:32 +0530

    day timetable

--- controllers/timetable/index.js
+++ controllers/timetable/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const timetableController = require('./timetable.controller');
 
-router.get('/student/:id',timetableController.getClassTimetable);
+router.get('/student',timetableController.getClassTimetable);
 router.get('/student/day',timetableController.getClassTimetableByDay);
 
 
--- controllers/timetable/timetable.controller.js
+++ controllers/timetable/timetable.controller.js
@@ -4,7 +4,7 @@ const facultyTimetable = require('../../models/facultyTimetable');
 const classTimetable = require('../../models/classTimetable');
 module.exports = {
     getClassTimetable: (req, res) => {
-        console.log('working',req.params.id);
+        console.log('working', req.params.id);
         // student.findOne({user_id:req.params.id},(err,studentFound)=>{
         //     if(err){
         //         res.status(404).json({error:true,error_msg:'Something went wrong',err})
@@ -12,7 +12,7 @@ module.exports = {
         //     else{
         //         classTimetable.findOne({course:studentFound.course,branch:studentFound.branch,year:studentFound.year,semester:studentFound.semester})
         //     }
-            
+
 
         // })
 
@@ -23,10 +23,33 @@ module.exports = {
         })
 
     },
-    getClassTimetableByDay:(req,res)=>{
-        classTimetable.find({"timetable.day":"monday"},(e,tt)=>{
-            if(e) throw e
-            res.json({tt});
+    getClassTimetableByDay: (req, res) => {
+
+        classTimetable.findOne({ "timetable.day": "monday" }, (e, tt) => {
+            if (e) {
+                res.json({ error: true, error_msg: 'Something went wrong', err: e })
+            }
+            else {
+                console.log(tt);
+                
+                let today = new Date('07-may-2019').getDay();
+
+                if(today == 0 || today == 6){
+
+                    res.json({leave:true,reason:'weekend',error:false})
+                }
+                else{
+                    res.json({error:false,leave:false,timetable:{
+                        course:tt.course,
+                        department:tt.department,
+                        year:tt.year,
+                        semester:tt.semester,
+                        section:tt.section,
+                        dayTimetable:tt.timetable[today-1]
+                    }})
+
+                }
+            }
         })
     }
 
