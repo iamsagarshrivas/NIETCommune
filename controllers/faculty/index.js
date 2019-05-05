5002de4f9e772345cc367f693fcd1fcec4ef00b0
sagar.shrivas <sagar.shrivas@geminisolutions.com>
2019-05-05 20:59:37 +0530

    student notices

--- controllers/faculty/index.js
+++ controllers/faculty/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const facultyController = require('./faculty.controller');
 
-router.get('/all-notices/:id',facultyController.getMyNotices);
+router.get('/notices/:id',facultyController.getMyNotices);
 router.get('/all-notices/details/:id',facultyController.getNoticeDetails);
 router.post('/generate-notice',facultyController.generateNotice);
 
